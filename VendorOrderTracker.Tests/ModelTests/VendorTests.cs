9bc694f969e5f6cf32f39993703e72ca23d4e6a7
Suzanne Schuber <suzanneschuber@gmail.com>
2023-07-22 12:27:20 -0700

    vendor test to add properties pass, update vendor model.

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VendorOrderTracker.Models;
+using System.Collections.Generic;
 
 namespace VendorOrderTracker.Tests
 {
@@ -14,5 +15,23 @@ namespace VendorOrderTracker.Tests
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
 
+    [TestMethod]
+
+    public void VendorProperties_UpdateVendorProperties_Vendor()
+    {
+      Vendor newVendor = new Vendor();
+
+      newVendor.VendorName = "Sample Vendor";
+      newVendor.VendorType = "Sample Type";
+      newVendor.VendorEmail = "sample@vendor.com";
+      newVendor.VendorPhoneNumber = "123-456-7890";
+      newVendor.Orders = new List<Order>();
+
+      Assert.AreEqual("Sample Vendor", newVendor.VendorName);
+      Assert.AreEqual("Sample Type", newVendor.VendorType);
+      Assert.AreEqual("sample@vendor.com", newVendor.VendorEmail);
+      Assert.AreEqual("123-456-7890", newVendor.VendorPhoneNumber);
+      Assert.IsNotNull(newVendor.Orders);
+    }
   }
 }
--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -4,6 +4,10 @@ namespace VendorOrderTracker.Models
 {
   public class Vendor
   {
-
+    public string VendorName { get; set; }
+    public string VendorType { get; set; }
+    public string VendorEmail { get; set; }
+    public string VendorPhoneNumber { get; set; }
+    public List<Order> Orders { get; set; }
   }
 }
