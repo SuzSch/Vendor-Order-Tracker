347522c6c75bc186cf566eadca2df8aa948bb8e1
Suzanne Schuber <suzanneschuber@gmail.com>
2023-07-22 13:24:34 -0700

    OrderProperties test pass, update Order class.

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VendorOrderTracker.Models;
 using System.Collections.Generic;
+using System;
 
 namespace VendorOrderTracker.Tests
 {
@@ -12,6 +13,24 @@ namespace VendorOrderTracker.Tests
     {
       Order newOrder = new Order();
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
+    [TestMethod]
+
+    public void OrderProperties_UpdateOrderProperties_Order()
+    {
+      Order newOrder = new Order();
+
+      newOrder.Title = "Winter Holiday 2023 Order";
+      newOrder.Description = "Two dozen of each holiday cookie";
+      newOrder.Price = 200;
+      newOrder.Date = new DateTime(2023, 12, 01);
+      
+
+      Assert.AreEqual("Winter Holiday 2023 Order", newOrder.Title);
+      Assert.AreEqual("Two dozen of each holiday cookie", newOrder.Description);
+      Assert.AreEqual(200, newOrder.Price);
+      Assert.AreEqual(new DateTime(2023, 12, 01), newOrder.Date);
     }
   }
 }
--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -1,9 +1,13 @@
 using System.Collections.Generic;
+using System;
 
 namespace VendorOrderTracker.Models
 {
   public class Order
   {
-
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public DateTime Date { get; set; }
   }
 }
