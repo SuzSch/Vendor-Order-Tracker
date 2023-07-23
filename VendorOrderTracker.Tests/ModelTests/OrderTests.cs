c989c0f4bad47a600871cae03f76660b7fa355a1
Suzanne Schuber <suzanneschuber@gmail.com>
2023-07-23 11:56:23 -0700

    WIP test and method for MakeOrder in the Order model.

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -21,16 +21,39 @@ namespace VendorOrderTracker.Tests
     {
       Order newOrder = new Order("Winter Holiday 2023 Order", "Two dozen of each holiday cookie", 200, new DateTime(2023, 12, 01));
 
-      newOrder.Title = "Winter Holiday 2023 Order";
-      newOrder.Description = "Two dozen of each holiday cookie";
-      newOrder.Price = 200;
-      newOrder.Date = new DateTime(2023, 12, 01);
-      
+      string title = "Winter Holiday 2023 Order";
+      string description = "Two dozen of each holiday cookie";
+      int price = 200;
+      date = new DateTime(2023, 12, 01);
+
 
       Assert.AreEqual("Winter Holiday 2023 Order", newOrder.Title);
       Assert.AreEqual("Two dozen of each holiday cookie", newOrder.Description);
       Assert.AreEqual(200, newOrder.Price);
       Assert.AreEqual(new DateTime(2023, 12, 01), newOrder.Date);
+    }
+
+    [TestMethod]
+
+    public void MakeOrder_MakeOrder_Order()
+    {
+      string title = "Winter Holiday 2023 Order";
+      string description = "Two dozen of each holiday cookie";
+      int price = 200;
+      date = new DateTime(2023, 12, 01);
+      Order newOrder = new Order("Winter Holiday 2023 Order", "Two dozen of each holiday cookie", 200, new DateTime(2023, 12, 01));
+
+      string newTitle = "Valentine's Day 2024 Order";
+      string newDescription = "10 dozen red velvet cupcakes";
+      int newPrice = 400;
+      newDate = new DateTime(2024, 2, 01);
+
+      newOrder.Title = newTitle;
+      newOrder.Description = newDescription;
+      newOrder.Price = newPrice;
+      newOrder.Date = newDate;
+      
+      string result = newItem.Description;
     }
   }
 }
