ea0a1daa395dd6ae357507b5e48abfee0d2478a5
Suzanne Schuber <suzanneschuber@gmail.com>
2023-07-22 21:14:41 -0700

    write and pass test for Find() method, update Vendor Class, add ClearAll method to Vendor class.

--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -4,17 +4,35 @@ namespace VendorOrderTracker.Models
 {
   public class Vendor
   {
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
     private static int lastVendorId = 0;
+    private static List<Vendor> _instances = new List<Vendor>();
 
-
-    public string VendorName { get; set; }
-    public string VendorType { get; set; }
-    public string VendorEmail { get; set; }
-    public string VendorPhoneNumber { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public int Id { get; }
     public List<Order> Orders { get; set; }
-    public int VendorId { get; }
 
-    
+    public Vendor(string name, string type, string email, string phoneNumber, int id)
+    {
+      Name = name;
+      Type = type;
+      Email = email;
+      PhoneNumber = phoneNumber;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order> { };
+    }
+
+    public static Vendor Find(int searchId)
+    {
+      return _instances[searchId - 1];
+    }
   }
 
 
